5a54fe52ec2752bed64b8f1dfab219e676a6bfe0
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-09-04 08:32:50 -0300

    feat: create user (POST)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import type { CreateUserDto } from './dtos/createUser.dto';
 
+
 @Controller('user')
 export class UserController {
     @Post()
     async createUser(@Body() createUser: CreateUserDto) {
-        return createUser;
+        return {
+            ...createUser,
+            password: undefined,
+        };
     }
 }
