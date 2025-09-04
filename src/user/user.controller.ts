ac71407f0c87ad9dea71a0cfdb69ec4c36572590
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-09-04 13:03:11 -0300

    feat: security on the password

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import type { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { get } from 'http';
+import { User } from './interfaces/user.interface';
 
 
 @Controller('user')
 export class UserController {
+
+    constructor(private readonly userService: UserService) {}
     @Post()
-    async createUser(@Body() createUser: CreateUserDto) {
-        return {
-            ...createUser,
-            password: undefined,
-        };
+    async createUser(@Body() createUser: CreateUserDto): Promise<User> {
+        return this.userService.createUser(createUser);
+    }
+
+    @Get()
+    async getAllUser(): Promise<User[]> {
+        return this.userService.getAllUser()
     }
 }
