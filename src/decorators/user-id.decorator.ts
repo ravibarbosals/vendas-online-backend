0ecafe1cd1853fddb91962605705a4762370b89b
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-09-18 13:14:10 -0300

    feat insert validation in email duplicated

--- src/decorators/user-id.decorator.ts
+++ src/decorators/user-id.decorator.ts
@@ -8,4 +8,4 @@ export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
 
 
     return loginPayload?.id;
-  });
+});
---
+++ src/migration/1758202502777-alter-table-user.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AlterTableUser1758202502777 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+        alter table public.user add unique(email);
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+        alter table public.user add unique(email);
+        `);
+    }
+
+}
