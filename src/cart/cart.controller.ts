6fcab01a735397891ebb01b6042617bd14b5f149
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-09-30 08:51:54 -0300

    feat: clear cart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
 import { CartEntity } from './entities/cart.entity';
@@ -6,6 +6,7 @@ import { InsertCartDTO } from './dtos/insert-cart.dto';
 import { CartService } from './cart.service';
 import { UserId } from 'src/decorators/user-id.decorator';
 import { ReturnCartDTO } from './dtos/return-card.dto';
+import { DeleteResult } from 'typeorm';
 
 @Roles(UserType.User, UserType.Admin)
 @Controller('cart')
@@ -20,6 +21,18 @@ export class CartController {
         @UserId() userId: number,
     ): Promise<ReturnCartDTO> {
         return new ReturnCartDTO(
-            await this.cartService.insertProductInCart(insertCart, userId));
+            await this.cartService.insertProductInCart(insertCart, userId)
+        );
+    }
+
+    @Get()
+    async findCartByUserId(@UserId() userId: number): Promise<ReturnCartDTO> {
+        return new ReturnCartDTO(
+            await this.cartService.findCartByUserId(userId, true));
+    }
+
+    @Delete()
+    async clearCart(@UserId() userId: number): Promise<DeleteResult> {
+        return this.cartService.clearCart(userId);
     }
 }
