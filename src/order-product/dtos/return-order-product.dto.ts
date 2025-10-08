9a7c88644fd2d1fcc0b69b84300ba9354eec250d
RAVI ALMEIDA BARBOSA LIMA <ravi.lima@SISTEMAFIEPE.LOCAL>
2025-10-08 10:55:09 -0300

    feat: insert returns and unit test in findOrderById

---
+++ src/order-product/dtos/return-order-product.dto.ts
@@ -0,0 +1,28 @@
+import { ReturnOrderDTO } from "../../order/dto/return-order.dto";
+import { ReturnProduct } from "../../product/dtos/return-product.dto";
+import { OrderProductEntity } from "../entities/order-product.entity";
+
+export class ReturnOrderProductDTO {
+    id: number;
+    orderId: number;
+    productId: number;
+    amount: number;
+    price: number;
+    order?: ReturnOrderDTO;
+    product?: ReturnProduct;
+
+
+    constructor(orderProduct: OrderProductEntity) {
+        this.id = orderProduct.id;
+        this.orderId = orderProduct.orderId;
+        this.productId = orderProduct.productId;
+        this.amount = orderProduct.amount;
+        this.price = orderProduct.price;
+        this.order = orderProduct.order 
+        ? new ReturnOrderDTO(orderProduct.order) 
+        : undefined;
+        this.product = orderProduct.product 
+        ? new ReturnProduct(orderProduct.product) 
+        : undefined
+    }
+}
